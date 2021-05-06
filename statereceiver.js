ecd657d192aff79d9402a1b2508fdf21ba76f1d8
Michael Yeates <michaeljyeates@gmail.com>
2021-05-06 10:18:49 +0100

    Fix block timestamp

--- statereceiver.js
+++ statereceiver.js
@@ -29,6 +29,23 @@ class StateReceiver {
 
     }
 
+    parseDate (fullStr) {
+        const [fullDate] = fullStr.split('.')
+        const [dateStr, timeStr] = fullDate.split('T')
+        const [year, month, day] = dateStr.split('-')
+        const [hourStr, minuteStr, secondStr] = timeStr.split(':')
+
+        const dt = new Date()
+        dt.setUTCFullYear(year)
+        dt.setUTCMonth(month - 1)
+        dt.setUTCDate(day)
+        dt.setUTCHours(hourStr)
+        dt.setUTCMinutes(minuteStr)
+        dt.setUTCSeconds(secondStr)
+
+        return dt.getTime()
+    }
+
     registerBlockHandler(h){
         this.block_handlers.push(h)
     }
@@ -161,7 +178,7 @@ class StateReceiver {
 
         let block_timestamp = null;
         if (block){
-            block_timestamp = new Date(block.timestamp.replace(['.000', '.500'], 'Z'));
+            block_timestamp = new Date(this.parseDate(block.timestamp.replace(['.000', '.500'], 'Z')));
         }
         else {
             block_timestamp = new Date();
